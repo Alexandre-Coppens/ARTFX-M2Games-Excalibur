09e8e67e867c611e869f59b9612c1afc654b9e90
alexandre.coppens <alexandre.coppens@artfx.fr>
2024-12-04 16:25:33 +0100

    [Add] Script to move Character + test Animations

--- Assets/Prog - Alexandre/Scripts/Player_Behaviour.cs
+++ Assets/Prog - Alexandre/Scripts/Player_Behaviour.cs
@@ -9,31 +9,47 @@ public class Player_Behaviour : MonoBehaviour
     [SerializeField] private float jumpForce = 1f;
     [SerializeField] private float jumpAirTime = 2f;
 
+    [Header("GroundCheck")]
+    [SerializeField] private float groundDifference = 1f;
+    [SerializeField] private float checkSize = 1f;
+    [SerializeField] private int groundLayer = 6;
+
+    [Header("Debug Jump")]
+    [SerializeField] private bool isOnGround = true;
+    [SerializeField] private bool isJumping;
+    [SerializeField] private bool canJump;
+    [SerializeField] private float currentAirJumpTime;
+
     [Header("Debug Components")]
     [SerializeField] private Player_Inputs inputs;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Animator animator;
 
     [Header("Debug Controller")]
     [SerializeField] private float movement;
     [SerializeField] private bool jumpPressed;
-    [SerializeField] private float currentAirJumpTime;
     [SerializeField] private bool attackPressed;
     [SerializeField] private bool interactPressed;
     [SerializeField] private bool throwPressed; 
+
+    
 
     void Start()
     {
         inputs = Player_Inputs.instance;
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         GetInputs();
+        CheckGround();
         Movements();
+        Animations();
     }
 
     private void GetInputs()
@@ -43,6 +59,21 @@ public class Player_Behaviour : MonoBehaviour
         attackPressed = inputs.attackPressed;
         interactPressed = inputs.interactionPressed;
         throwPressed = inputs.throwPressed;
+    }
+
+    private void CheckGround()
+    {
+        Collider2D[] hit = Physics2D.OverlapCircleAll(transform.position + new Vector3(0, groundDifference), checkSize);
+        bool _test = false;
+        foreach (Collider2D col in hit)
+        {
+            if (col.gameObject.layer == groundLayer) { _test = true; break; }
+        }
+        if (_test && rb.velocity.y < 0.1f)
+        {
+            isOnGround = true;
+        }
+        else { isOnGround = false; }
     }
 
     private void Movements()
@@ -56,7 +87,39 @@ public class Player_Behaviour : MonoBehaviour
     {
         if (jumpPressed)
         {
-
+            if (!isJumping && isOnGround)
+            {
+                animator.SetTrigger("Jump");
+                isJumping = true;
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                currentAirJumpTime = 0;
+            }
+            else if (isJumping && currentAirJumpTime <= jumpAirTime)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                currentAirJumpTime += Time.deltaTime;
+            }
+            else if(isJumping && currentAirJumpTime > jumpAirTime) 
+            { 
+                isJumping = false;
+            }
         }
+        else
+        {
+            isJumping = false;
+        }
+    }
+
+    private void Animations()
+    {
+        animator.SetFloat("velX", rb.velocity.x);
+        animator.SetFloat("velY", rb.velocity.y);
+        animator.SetBool("isOnGround", isOnGround);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position + new Vector3(0, groundDifference), checkSize);
     }
 }
